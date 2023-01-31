d5cfbf372beff4de851bc93baa7941cfef9dea7d
Kaspar Tilk <kaspartilk2@gmail.com>
2023-01-31 11:20:59 +0200

    Update .NET version and Nuget packages

--- Web/App_Start/NinjectWebCommon.cs
+++ Web/App_Start/NinjectWebCommon.cs
@@ -1,25 +1,26 @@
-using System;
-using System.Web;
-using DAL;
-using DAL.Helpers;
-using DAL.Interfaces;
-using DAL.Repositories;
-using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-using Ninject;
-using Ninject.Web.Common;
-using Web;
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Web.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Web.App_Start.NinjectWebCommon), "Stop")]
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
-
-namespace Web
+namespace Web.App_Start
 {
+    using System;
+    using System.Web;
+
+    using DAL;
+    using DAL.Helpers;
+    using DAL.Interfaces;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
+
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
-        /// Starts the application
+        /// Starts the application.
         /// </summary>
         public static void Start() 
         {
@@ -27,7 +28,7 @@ namespace Web
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -35,7 +36,7 @@ namespace Web
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -47,7 +48,6 @@ namespace Web
             {
                 kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-
                 RegisterServices(kernel);
                 return kernel;
             }
@@ -68,6 +68,6 @@ namespace Web
             kernel.Bind<EFRepositoryFactories>().To<EFRepositoryFactories>().InSingletonScope();
             kernel.Bind<IEFRepositoryProvider>().To<EFRepositoryProvider>().InRequestScope();
             kernel.Bind<IUOW>().To<UOW>().InRequestScope();
-        }        
+        }
     }
 }
